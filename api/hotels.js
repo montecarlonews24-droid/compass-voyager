1af2438f6a600d01aae0cf9d8a545448cb9f1f35
montecarlonews24-droid <montecarlo.news24@gmail.com>
2026-07-07 07:03:01 +0300

    Add files via upload

--- api/hotels.js
+++ api/hotels.js
@@ -1,4 +1,9 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+
   const { path, ...query } = req.query;
 
   if (!path) {
@@ -36,7 +41,6 @@ export default async function handler(req, res) {
       }
     });
     const data = await response.json();
-    res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(response.status).json(data);
   } catch (err) {
     res.status(500).json({ error: 'Proxy request failed', details: err.message });
